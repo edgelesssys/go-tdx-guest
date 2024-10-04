e5eafdf541887b9f665e315e06086e8a383cd3f0
Tom Dohrmann <erbse.13@gmx.de>
2024-10-04 09:03:14 +0200

    support optionally validating SGXType
    This patch lays the groundwork for validating extensions inside the PCK
    certificate beyond what's needed to verify that a quote is valid. This
    patch adds an optional validation option to check the SGX type. This
    can be used to differentiate platforms with logical and cryptographic
    integrity. In the future, we could validate other fields e.g. "SMT
    Enabled".

--- validate/validate.go
+++ validate/validate.go
@@ -17,11 +17,15 @@ package validate
 
 import (
 	"bytes"
+	"crypto/x509"
 	"encoding/binary"
 	"encoding/hex"
+	"encoding/pem"
+	"errors"
 	"fmt"
 
 	"github.com/google/go-tdx-guest/abi"
+	"github.com/google/go-tdx-guest/pcs"
 	ccpb "github.com/google/go-tdx-guest/proto/checkconfig"
 	pb "github.com/google/go-tdx-guest/proto/tdx"
 	vr "github.com/google/go-tdx-guest/verify"
@@ -51,6 +55,7 @@ const (
 type Options struct {
 	HeaderOptions      HeaderOptions
 	TdQuoteBodyOptions TdQuoteBodyOptions
+	PCKOptions         PCKOptions
 }
 
 // HeaderOptions represents validation options for a TDX attestation Quote Header.
@@ -95,6 +100,12 @@ type TdQuoteBodyOptions struct {
 	// EnableTdMigratableCheck determines whether the MIGRATABLE bit (bit 29) in TD_ATTRIBUTES is checked.
 	// If true, the MIGRATABLE bit must be 0. If false, the MIGRATABLE bit is not checked.
 	EnableTdMigratableCheck bool
+}
+
+// PCKOptions represents validation options for the PCK certificate in side a TDX attestation.
+type PCKOptions struct {
+	// SgxType is the expected SGXType. Not checked if nil.
+	SgxType *pcs.SGXType
 }
 
 func lengthCheck(name string, length int, value []byte) error {
@@ -439,6 +450,42 @@ func validateTdAttributes(value []byte, fixed1, fixed0 uint64, enableTdDebugChec
 	return nil
 }
 
+func validatePck(quote any, opts *PCKOptions) error {
+	// Extract the PCK. Both QuoteV4 and QuoteV5 expose the signed data containing
+	// the PCK certificate chain via the same auth data structure.
+	var signedData *pb.Ecdsa256BitQuoteV4AuthData
+	switch q := quote.(type) {
+	case *pb.QuoteV4:
+		signedData = q.GetSignedData()
+	case *pb.QuoteV5:
+		signedData = q.GetSignedData()
+	default:
+		return fmt.Errorf("unsupported quote type: %T", quote)
+	}
+	certChainBytes := signedData.GetCertificationData().GetQeReportCertificationData().GetPckCertificateChainData().GetPckCertChain()
+	if certChainBytes == nil {
+		return errors.New("PCK certificate chain is empty")
+	}
+	pck, rem := pem.Decode(certChainBytes)
+	if pck == nil || len(rem) == 0 || pck.Type != "CERTIFICATE" {
+		return errors.New("incomplete PCK Certificate chain found, should contain 3 concatenated PEM-formatted 'CERTIFICATE'-type block (PCK Leaf Cert||Intermediate CA Cert||Root CA Cert)")
+	}
+	pckCert, err := x509.ParseCertificate(pck.Bytes)
+	if err != nil {
+		return fmt.Errorf("could not interpret PCK leaf certificate DER bytes: %v", err)
+	}
+
+	// Validate the PCK.
+	exts, err := pcs.PckCertificateExtensions(pckCert)
+	if err != nil {
+		return fmt.Errorf("could not get PCK certificate extensions: %v", err)
+	}
+	if opts.SgxType != nil && *opts.SgxType != exts.SGXType {
+		return fmt.Errorf("PCK extension SGXType is %d. Expect %d", *opts.SgxType, exts.SGXType)
+	}
+	return nil
+}
+
 // TdxQuote validates fields of the protobuf representation of an attestation Quote
 // against expectations depending on supported quote formats - QuoteV4, QuoteV5.
 // Does not check the attestation certificates or signature.
@@ -468,6 +515,7 @@ func TdxQuote(quote any, options *Options) error {
 		minVersionCheck(quote, options),
 		validateXfam(xfam, xfamFixed1, xfamFixed0),
 		validateTdAttributes(tdAttributes, tdAttributesFixed1, tdAttributesFixed0, options.TdQuoteBodyOptions.EnableTdDebugCheck, options.TdQuoteBodyOptions.EnableTdMigratableCheck),
+		validatePck(quote, &options.PCKOptions),
 	)
 }
 
